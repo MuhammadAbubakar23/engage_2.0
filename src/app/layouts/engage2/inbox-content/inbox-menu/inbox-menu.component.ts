ceba2717d71fa974124c3719f6a9e2991bf89eac
TaimoorWajid <syedtaimoor26@gmail.com>
2023-07-07 14:01:06 +0500

    menu changes for tppl

--- src/app/layouts/engage2/inbox-content/inbox-menu/inbox-menu.component.ts
+++ src/app/layouts/engage2/inbox-content/inbox-menu/inbox-menu.component.ts
@@ -41,43 +41,45 @@ export class InboxMenuComponent implements OnInit {
       .getUnRespondedCount()
       .subscribe((res) => {
         
-        this.UnResponded = res.totalCount
+        // this.UnResponded = res.totalCount
         res.platformCount.forEach((platform:any) => {
-          if(platform.platform == 'Facebook'){
-            this.FbUnResponded = platform.count;
-          }
-          if(platform.platform == 'Instagram'){
-            this.InstaUnResponded = platform.count;
-          }
-          if(platform.platform == 'Twitter'){
-            this.TwitterUnResponded = platform.count;
-          }
-          if(platform.platform == 'LinkedIn'){
-            this.LinkedInUnResponded = platform.count;
-          }
-          if(platform.platform == 'Youtube'){
-            this.YoutubeUnResponded = platform.count;
-          }
-          if(platform.platform == 'SMS'){
-            this.SmsUnResponded = platform.count;
-          }
+          // if(platform.platform == 'Facebook'){
+          //   this.FbUnResponded = platform.count;
+          // }
+          // if(platform.platform == 'Instagram'){
+          //   this.InstaUnResponded = platform.count;
+          // }
+          // if(platform.platform == 'Twitter'){
+          //   this.TwitterUnResponded = platform.count;
+          // }
+          // if(platform.platform == 'LinkedIn'){
+          //   this.LinkedInUnResponded = platform.count;
+          // }
+          // if(platform.platform == 'Youtube'){
+          //   this.YoutubeUnResponded = platform.count;
+          // }
+          // if(platform.platform == 'SMS'){
+          //   this.SmsUnResponded = platform.count;
+          // }
           if(platform.platform == 'WhatsApp'){
             this.WaUnResponded = platform.count;
+            this.UnResponded = res.totalCount
           }
-          if(platform.platform == 'WebChat'){
-            this.WebchatUnResponded = platform.count;
-          }
-          if(platform.platform == 'Email'){
+          // if(platform.platform == 'WebChat'){
+          //   this.WebchatUnResponded = platform.count;
+          // }
+          // if(platform.platform == 'Email'){
             
-            this.EmailUnResponded = platform.count;
-          }
+          //   this.EmailUnResponded = platform.count;
+          // }
           if(platform.platform == 'OfficeEmail'){
             
             this.OfficeEmailUnResponded = platform.count;
+            this.UnResponded = res.totalCount
           }
-          if(platform.platform == 'PlayStore'){
-            this.PlaystoreUnResponded = platform.count;
-          }
+          // if(platform.platform == 'PlayStore'){
+          //   this.PlaystoreUnResponded = platform.count;
+          // }
         });
         
       });
@@ -85,86 +87,90 @@ export class InboxMenuComponent implements OnInit {
       this.Subscription = this.updateListService.receiveList().subscribe((res) => {
       
       res.forEach((platform:any) => {
-        this.UnResponded = platform.unrespondedCount + this.UnResponded;
-        if(platform.platform == 'Facebook'){
-          this.FbUnResponded = platform.unrespondedCount + this.FbUnResponded;
-        }
-        if(platform.platform == 'Instagram'){
-          this.InstaUnResponded = platform.unrespondedCount + this.InstaUnResponded;
-        }
-        if(platform.platform == 'Twitter'){
-          this.TwitterUnResponded = platform.unrespondedCount + this.TwitterUnResponded;
-        }
-        if(platform.platform == 'LinkedIn'){
-          this.LinkedInUnResponded = platform.unrespondedCount + this.LinkedInUnResponded;
-        }
-        if(platform.platform == 'Youtube'){
-          this.YoutubeUnResponded = platform.unrespondedCount + this.YoutubeUnResponded;
-        }
-        if(platform.platform == 'SMS'){
-          this.SmsUnResponded = platform.unrespondedCount + this.SmsUnResponded;
-        }
+        // this.UnResponded = platform.unrespondedCount + this.UnResponded;
+        // if(platform.platform == 'Facebook'){
+        //   this.FbUnResponded = platform.unrespondedCount + this.FbUnResponded;
+        // }
+        // if(platform.platform == 'Instagram'){
+        //   this.InstaUnResponded = platform.unrespondedCount + this.InstaUnResponded;
+        // }
+        // if(platform.platform == 'Twitter'){
+        //   this.TwitterUnResponded = platform.unrespondedCount + this.TwitterUnResponded;
+        // }
+        // if(platform.platform == 'LinkedIn'){
+        //   this.LinkedInUnResponded = platform.unrespondedCount + this.LinkedInUnResponded;
+        // }
+        // if(platform.platform == 'Youtube'){
+        //   this.YoutubeUnResponded = platform.unrespondedCount + this.YoutubeUnResponded;
+        // }
+        // if(platform.platform == 'SMS'){
+        //   this.SmsUnResponded = platform.unrespondedCount + this.SmsUnResponded;
+        // }
         if(platform.platform == 'WhatsApp'){
           this.WaUnResponded = platform.unrespondedCount + this.WaUnResponded;
+          this.UnResponded = platform.unrespondedCount + this.UnResponded;
         }
-        if(platform.platform == 'WebChat'){
-          this.WebchatUnResponded = platform.unrespondedCount + this.WebchatUnResponded;
-        }
-        if(platform.platform == 'Email'){
+        // if(platform.platform == 'WebChat'){
+        //   this.WebchatUnResponded = platform.unrespondedCount + this.WebchatUnResponded;
+        // }
+        // if(platform.platform == 'Email'){
           
-          this.EmailUnResponded = platform.unrespondedCount + this.EmailUnResponded;
-        }
+        //   this.EmailUnResponded = platform.unrespondedCount + this.EmailUnResponded;
+        // }
         if(platform.platform == 'OfficeEmail'){
           
           this.OfficeEmailUnResponded = platform.unrespondedCount + this.OfficeEmailUnResponded;
+          
         }
-        if(platform.platform == 'PlayStore'){
-          this.PlaystoreUnResponded = platform.unrespondedCount + this.PlaystoreUnResponded;
-        }
+        // if(platform.platform == 'PlayStore'){
+        //   this.PlaystoreUnResponded = platform.unrespondedCount + this.PlaystoreUnResponded;
+        // }
       });
       });
   }
 
   getAllChannelsUnrespondedCounts(){
     this.commonService.GetAllChannelsUnrespondedCount().subscribe((res:any)=>{
-      this.UnResponded = res.totalCount
+      // this.UnResponded = res.totalCount
       res.platformCount.forEach((platform:any) => {
 
-        if(platform.platform == 'Facebook'){
-          this.FbUnResponded = platform.count;
-        }
-        if(platform.platform == 'Instagram'){
-          this.InstaUnResponded = platform.count;
-        }
-        if(platform.platform == 'Twitter'){
-          this.TwitterUnResponded = platform.count;
-        }
-        if(platform.platform == 'LinkedIn'){
-          this.LinkedInUnResponded = platform.count;
-        }
-        if(platform.platform == 'Youtube'){
-          this.YoutubeUnResponded = platform.count;
-        }
-        if(platform.platform == 'SMS'){
-          this.SmsUnResponded = platform.count;
-        }
+        // if(platform.platform == 'Facebook'){
+        //   this.FbUnResponded = platform.count;
+        // }
+        // if(platform.platform == 'Instagram'){
+        //   this.InstaUnResponded = platform.count;
+        // }
+        // if(platform.platform == 'Twitter'){
+        //   this.TwitterUnResponded = platform.count;
+        // }
+        // if(platform.platform == 'LinkedIn'){
+        //   this.LinkedInUnResponded = platform.count;
+        // }
+        // if(platform.platform == 'Youtube'){
+        //   this.YoutubeUnResponded = platform.count;
+        // }
+        // if(platform.platform == 'SMS'){
+        //   this.SmsUnResponded = platform.count;
+        // }
         if(platform.platform == 'WhatsApp'){
           this.WaUnResponded = platform.count;
         }
-        if(platform.platform == 'WebChat'){
-          this.WebchatUnResponded = platform.count;
-        }
-        if(platform.platform == 'Email'){
+        // if(platform.platform == 'WebChat'){
+        //   this.WebchatUnResponded = platform.count;
+        // }
+        // if(platform.platform == 'Email'){
           
-          this.EmailUnResponded = platform.count;
-        }
+        //   this.EmailUnResponded = platform.count;
+        // }
         if(platform.platform == 'OfficeEmail'){
-          
           this.OfficeEmailUnResponded = platform.count;
         }
-        if(platform.platform == 'PlayStore'){
-          this.PlaystoreUnResponded = platform.count;
-        }
+        // if(platform.platform == 'PlayStore'){
+        //   this.PlaystoreUnResponded = platform.count;
+        // }
+
+        // for TPPL only
+        this.UnResponded = this.WaUnResponded + this.OfficeEmailUnResponded
       });
     });
   }
