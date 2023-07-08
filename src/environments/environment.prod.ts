aa61c895b48a95cdc592a260c904406f44042d1c
TaimoorWajid <syedtaimoor26@gmail.com>
2023-07-08 19:17:42 +0500

    env urls changed as per TPPL requirment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -16,7 +16,7 @@ export const environment = {
     types: { local: 'LS' },
   },
   title: 'abc',
-  IdentityBaseUrl:  'https://identity-engage.enteract.app/api', // 'https://tpplidp.360scrm.com/api', // 'http://localhost:5036/api',
+  IdentityBaseUrl: 'https://tpplidp.360scrm.com/api',
   InstaBaseUrl: 'https://insta.360scrm.com/api/', // other then post and data
   FbBaseUrl: 'https://face.360scrm.com/api/',
   YoutubeBaseUrl: 'https://tube.360scrm.com/api/',
@@ -25,12 +25,10 @@ export const environment = {
   SmsBaseUrl: 'https://text.360scrm.com/api/',
   WhatsappBaseUrl: 'https://whats.360scrm.com/api/',
   WebChatBaseUrl: 'https://chat.360scrm.com/api/',
-  CommonBaseUrl:  'https://common-engage.enteract.app/api/', // 'https://tpplcom.360scrm.com/api/', 
+  CommonBaseUrl: 'https://tpplcom.360scrm.com/api/', 
   CCMSURL: 'https://haccms.ibex.co/jomo/api/',
 
   JomoAccessToken: '407ecdb2308d5cc24e9f5d24a779e4a3151357bb',
-  // for testing purpose 
-  // consoleBaseUrl: 'https://10.111.32.97:45458/api',
   consoleBaseUrl: 'https://console-engage.enteract.app/api/',
 
   links: {
