944534ecd78b495892e3a7aff67a9cfba27a4d11
TaimoorWajid <syedtaimoor26@gmail.com>
2023-07-12 17:53:10 +0500

    email small bug fixes in tppl only

--- src/app/shared/Guards/responder-guard.guard.ts
+++ src/app/shared/Guards/responder-guard.guard.ts
@@ -19,16 +19,16 @@ export class ResponderGuardGuard implements CanDeactivate<unknown> {
     currentState: RouterStateSnapshot,
     nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       
-      // if (
-      //   localStorage.getItem('assignedProfile') == null ||
-      //   localStorage.getItem('assignedProfile') == '' ||
-      //   localStorage.getItem('assignedProfile') == undefined
-      // ){
-      //   return this.canDeactivateFlag;
-      // } else {
-      //   alert('Please complete the querry first')
-      //   return false
-      // }
+      if (
+        localStorage.getItem('assignedProfile') == null ||
+        localStorage.getItem('assignedProfile') == '' ||
+        localStorage.getItem('assignedProfile') == undefined
+      ){
+        return this.canDeactivateFlag;
+      } else {
+        alert('Please complete the querry first')
+        return false
+      }
       return this.canDeactivateFlag;
       
   }
