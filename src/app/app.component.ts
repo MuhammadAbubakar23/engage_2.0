7d1cb511bb980abe47f5a4eb9fd72dc17ab2f304
TaimoorWajid <syedtaimoor26@gmail.com>
2023-07-26 01:40:23 +0500

    remaining part merged of Sir Rizwan's code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
+import { Toaster } from './layouts/engage2/toaster/toaster';
+import { ToasterService } from './layouts/engage2/toaster/toaster.service';
 import { SignalRService } from './services/SignalRService/signal-r.service';
 import { CommonDataService } from './shared/services/common/common-data.service';
 
@@ -10,18 +12,31 @@ import { CommonDataService } from './shared/services/common/common-data.service'
 })
 export class AppComponent {
 
-  
-  
+toasters: Toaster[] = [];
   title = 'Enteract.Engage2.0';
 
   constructor(private signalRService: SignalRService,
     private router: Router,
-    private commonService : CommonDataService) {
+    private commonService : CommonDataService,
+    private toaster: ToasterService) {
       
     
   }
 
+  remove(index: number) {
+    console.log(index);
+    this.toasters = this.toasters.filter((v, i) => i !== index);
+    //this.toasts.splice(index, 1);
+  }
   ngOnInit() {
+    this.toaster.toaster$
+      .subscribe(toaster => {
+        if(toaster!==null){
+          this.toasters = [toaster, ...this.toasters];
+          setTimeout(() => this.toasters.pop(), toaster.delay || 6000);
+        }
+        
+      });
     // if (this.signalRService.hubconnection == undefined) {
     //   this.commonService.SignOut().subscribe(()=>{
     //     localStorage.clear();
