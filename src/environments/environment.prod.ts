344353f878a230fd2453adf3b20ba78b557ea746
TaimoorWajid <syedtaimoor26@gmail.com>
2023-07-27 20:52:48 +0500

    changes as per KE requirement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -16,7 +16,7 @@ export const environment = {
     types: { local: 'LS' },
   },
   title: 'abc',
-  IdentityBaseUrl:  'https://identity-engage.enteract.app/api', // 'https://tpplidp.360scrm.com/api', // 'http://localhost:5036/api',
+  IdentityBaseUrl:  'https://idtservice.enteract.live/api', // 'https://tpplidp.360scrm.com/api', // 'http://localhost:5036/api',
   InstaBaseUrl: 'https://insta.360scrm.com/api/', // other then post and data
   FbBaseUrl: 'https://face.360scrm.com/api/',
   YoutubeBaseUrl: 'https://tube.360scrm.com/api/',
@@ -25,7 +25,7 @@ export const environment = {
   SmsBaseUrl: 'https://text.360scrm.com/api/',
   WhatsappBaseUrl: 'https://whats.360scrm.com/api/',
   WebChatBaseUrl: 'https://chat.360scrm.com/api/',
-  CommonBaseUrl:  'https://common-engage.enteract.app/api/', // 'https://tpplcom.360scrm.com/api/', 
+  CommonBaseUrl:  'https://comservices.enteract.live/api/',
   CCMSURL: 'https://haccms.ibex.co/jomo/api/',
 
   JomoAccessToken: '407ecdb2308d5cc24e9f5d24a779e4a3151357bb',
