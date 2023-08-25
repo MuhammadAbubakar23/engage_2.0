d52fca5bdf6987a94ab45fecdbea93e9ba06bafd
Taimoor Wajid <taimoor.wajid@ibex.co>
2023-08-25 15:26:32 +0000

    Revert "Merge branch 'alihaider4' of https://gitlab1.ibex.co/enteract/ui-engage-2.0 into respondermodule"
    This reverts commit 561b57d03210d7c31088088d4c356e9aaa359522

--- src/app/modules/console/components/skills/create-skills/create-skills.component.ts
+++ src/app/modules/console/components/skills/create-skills/create-skills.component.ts
@@ -1,32 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import {
-   ReactiveFormsModule, 
-  FormsModule,  
-  UntypedFormBuilder,
-   UntypedFormControl, 
-   UntypedFormGroup, 
-   Validators ,FormControl,FormControlName,} from '@angular/forms';
+import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { AddSkillMembersComponent } from '../add-skill-members/add-skill-members.component';
 
 @Component({
   selector: 'app-create-skills',  
   standalone:true,
-  imports:[CommonModule, ReactiveFormsModule, FormsModule, AddSkillMembersComponent],
+  imports:[CommonModule, ReactiveFormsModule, AddSkillMembersComponent],
   templateUrl: './create-skills.component.html',
   styleUrls: ['./create-skills.component.scss']
 })
 export class CreateSkillsComponent implements OnInit {
-  toastermessage=false
-  AlterMsg="Team Information Add Successfully"
+
   isActive=false;
-  text:any='Team Name'
-isChecked:boolean=false
-selectedIds:any[]=[]
-allselected:boolean=false
-showIcon:boolean=false;
-fillter:any="Roles"
-sort:any=''
+
   userForm : UntypedFormGroup = new UntypedFormGroup({
     teamname : new UntypedFormControl(),
     description : new UntypedFormControl(),
@@ -55,111 +42,11 @@ sort:any=''
   }
 
   onSubmit(){
-    debugger
-    let data={
-      "name":this.userForm.value,
-      "assignment":this.assignment,
-      "availability":this.availability,
-    }
-    console.log("this.userForm.value===>",data);
-    this.userForm.reset()
+    console.log("this.userForm.value===>",this.userForm.value);
   }
   
   AddTeamMembers(){
     this.isActive=!this.isActive;
   }
 
-
-
-
-
-
-
-  // the listing of teams and Member
-  teamMembersList:any[]=[
-    {
-      imageURI:"../../../../../../assets/images/avatar-16.jpg",
-      id:1,
-      name:"Ahamad Khab",
-      EMPid:76777,
-      role:"Agent",
-      team:"Support",
-    },
-    {
-      imageURI:"../../../../../../assets/images/avatar-16.jpg",
-      id:2,
-      name:"Ali Khan",
-      EMPid:8989,
-      role:"Admin",
-      team:"VW Tach"
-    },
-    {
-      imageURI:"../../../../../../assets/images/avatar-16.jpg",
-      id:3,
-      name:"Usman Khan",
-      EMPid : 9877,
-      role:"User ",
-      team:"Jazz"
-    }
-  ]
-  isSelected(id:number){
-    return this.selectedIds.indexOf (id)>=0 }
-    toggleselection(id:number){
-      const index =this.selectedIds.findIndex((x)=>x==id)
-      if(index>=0){
-        this.selectedIds.splice(index,1)
-      }
-      else{
-        this.selectedIds.push(id)
-      }
-      if(this.selectedIds.length>=0){
-     this.showIcon=true
-   this.isChecked=false
-   
-      }
-      else{
-        this.showIcon=false
-        this.allselected=false
-        this.selectedIds=[]
-      }
-    }
-    refresh(){
-      this.showIcon=false
-      this.selectedIds=[]
-      this.allselected=false
-      this.isChecked=false
-    }
-
-    selectedunselectedAll(){
-      this.allselected=!this.allselected
-      this.isChecked=!this.isChecked
-      if(this.isChecked){
-        this.selectedIds=this.teamMembersList.map(item=>item.id)
-        console.log("this.selectedIds===>",this.selectedIds)
-      }
-      else{
-        this.selectedIds=[]
-      }
-      if(this.selectedIds.length>=0){
-        this.showIcon=true
-      }
-      else{
-        this.showIcon=false
-        this.selectedIds=[]
-        this.isChecked=false
-      }
-    }
-    sortedBy(data:string){
-      // this.teamMembersList.sort((a,b)=>a[data]>b[data] ? 1 :-1)
-    } 
-    assignment:any=''
-    getAssignmentByid(id:any){
-    this.assignment=id
-
-    }
-    availability:any=''
-   getAvailability(id:any){
-    this.availability=id
-
-   }
 }
