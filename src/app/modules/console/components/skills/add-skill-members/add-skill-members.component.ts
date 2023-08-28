c58a0cb451665c24409c7ddbf6e8a55a403b4bfb
Taimoor Wajid <taimoor.wajid@ibex.co>
2023-08-28 07:51:32 +0000

    Revert "Revert "Merge branch 'alihaider4' of..."
    This reverts commit d52fca5bdf6987a94ab45fecdbea93e9ba06bafd

--- src/app/modules/console/components/skills/add-skill-members/add-skill-members.component.ts
+++ src/app/modules/console/components/skills/add-skill-members/add-skill-members.component.ts
@@ -1,16 +1,94 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl,FormGroup,FormControlName,Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-add-skill-members',
   standalone:true,
+  imports:[CommonModule ,ReactiveFormsModule,FormsModule],
   templateUrl: './add-skill-members.component.html',
   styleUrls: ['./add-skill-members.component.scss']
 })
 export class AddSkillMembersComponent implements OnInit {
-
+  isSelectedIds:any[]=[];
+  isSelectedIdstostring:string='';
+  numbers:any;
+  search:any='User name'
+  
+  AlterMsg='Team Members Added Successfully'
+  toastermessage:boolean=false
   constructor() { }
+  teamMemberForm=new FormGroup({
+ numbersIds:new FormControl('',Validators.required)
+  })
 
   ngOnInit(): void {
   }
+  
+  teamMemberDetails:any[]=[
+    {
+      imageUrl:"../../../../../../assets/images/avatar-16.jpg",
+      name:"Usman Khan",
+      id:1
+    },
+    {
+      imageUrl:"../../../../../../assets/images/avatar-11.jpg",
+      name:"Ahmad Khan",
+      id:2
 
+    },
+    {
+      imageUrl:"../../../../../../assets/images/avatar-11.jpg",
+      name:"Awais Khan",
+      id:3
+    },
+    {
+      imageUrl:"../../../../../../assets/images/avatar-10.jpg",
+      name:"Umair Khan",
+      id:4
+    },
+    {
+      imageUrl:"../../../../../../assets/images/avatar-16.jpg",
+      name:"Ali khan",
+      id:5
+    },
+    {
+      imageUrl:"../../../../../../assets/images/avatar-11.jpg",
+      name:"test user",
+      id:6
+    },
+     {
+      imageUrl:"../../../../../../assets/images/avatar-10.jpg",
+      name:"Abu Bakar",
+      id:7
+
+    }
+  ]
+  
+  save(){
+
+    this.isSelectedIdstostring=this.isSelectedIds.toString();
+    this.teamMemberForm.value.numbersIds=this.isSelectedIdstostring;
+    console.log("this.teamMemberForm===>",this.teamMemberForm.value.numbersIds)
+  }
+
+isSelected(id:number):boolean{
+  return this.isSelectedIds.indexOf(id)>=1
+}
+toggleSelectionIds(id:number):void{
+  debugger
+  const index =this.isSelectedIds.findIndex((x:any)=>x==id)
+  if(index >=0){
+    this.isSelectedIds.splice(index,1)
+  }
+  else{
+    this.isSelectedIds.push(id)
+  }
+ console.log("this.isSelectedIds==>",this.isSelectedIds)
+}
+
+
+closeToaster(){
+  this.toastermessage=false
+}
 }
