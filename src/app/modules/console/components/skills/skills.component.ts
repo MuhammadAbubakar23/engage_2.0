e0d8579cb16845185531470656afcec6f195be6b
ali.haider4 <ali.haider4@ibex.co>
2023-08-29 14:37:12 +0500

    changing in skills component

--- src/app/modules/console/components/skills/skills.component.ts
+++ src/app/modules/console/components/skills/skills.component.ts
@@ -12,52 +12,49 @@ import { FormsModule } from '@angular/forms';
 })
 export class SkillsComponent implements OnInit {
   search:any='VW'
-  fillter:any="Roles"
+  fillter:any="Sales"
   selectedIds:any[]=[]
   isChecked:boolean=false
   showIcon:boolean=false
   allSelected:boolean=false
   Page:any=1
-  itemperpage:any=10;
+  itemperpage:any=7;
+  isSelectedDepartment:any='';
+  toastermessage:boolean=false
+  isDepartmentArray:any[]=[]
   constructor(private headerService: HeaderService) { }
 
   ngOnInit(): void {
     // Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     // .forEach(tooltipNode => new Tooltip(tooltipNode));
+    this.userDetails.forEach((x)=>{
+      this.isSelectedDepartment=x.department
+      console.log("this.isSelectedDepartment==>",this.isDepartmentArray)
+      this.isDepartmentArray.push(this.isSelectedDepartment)
+    });
+ 
     
   }
   updatevalue(string:any){
 
     this.headerService.updateMessage(string);
   }
-   userDetails:any[]=[
 
-    { id: 1, teams:"vw",userid:333,department:"information technology"},
+  
+   userDetails:any[]=[
+    { id: 1, teams:"vw",userid:333,department:"Information technology"},
     { id: 2, teams:"special Projects",userid:3434,department:"Sales"},
     { id:3, teams:"Corporte Team",userid:3422,department:"Account,Support"},
-    { id:4, teams:"lead Management",userid:9988,department:"All"}
+    { id:4, teams:"lead Management",userid:9988,department:"All"},
+    { id:5, teams:"PK Sport",userid:9988,department:"All"},
+    { id:6, teams:"HR Management",userid:9988,department:"All"},
+    { id:7, teams:"IT Managment",userid:9908,department:"IT Managemnet"},
+
    ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
    isSelected(id:number){
-    debugger
     return this.selectedIds.indexOf(id)>=0;
    }
    toggleSelection(id:number){
-    debugger
       const index= this.selectedIds.findIndex((x)=>x==id);
      if(index>=0){
         this.selectedIds.splice(index,1)
@@ -65,7 +62,6 @@ export class SkillsComponent implements OnInit {
     else{
       this.selectedIds.push(id)
     }
-
     if(this.selectedIds.length>=0){
       this.showIcon=true
       this.allSelected=true
@@ -76,17 +72,19 @@ export class SkillsComponent implements OnInit {
       this.showIcon=false
       this.selectedIds=[];
       this.allSelected=false
+    }
 
-  
+    if(this.selectedIds.length==this.itemperpage){
+      this.isChecked=true
+      this.refresh()
     }
    }
-
 
    isSectedAll(){
     this.allSelected=!this.allSelected;
     this.isChecked=!this.isChecked;
     if(this.isChecked){
-      this.selectedIds=this.  userDetails.map(item=>item.id);
+      this.selectedIds=this.userDetails.map(item=>item.id);
     }
     else{
       this.selectedIds=[];
@@ -103,7 +101,49 @@ export class SkillsComponent implements OnInit {
     this.showIcon=false,
     this.allSelected=false
     this.isChecked=false
-    this.selectedIds=[]
+    this.selectedIds=[];
   }
+isDecsendingorder:boolean=false
 
+isDeccsendingsort:string=''
+
+sortedBy(data:string){
+
+  this.isDecsendingorder=!this.isDecsendingorder
+ this.userDetails.sort((a:any,b:any)=>{
+  if(a[data]<b[data]){
+   
+    return this.isDecsendingorder ? 1:-1;
+  }
+  if(a[data]>b[data]){
+   
+    return this.isDecsendingorder ? -1:1;
+  }
+  return 0
+ })
+}
+deleted(id:number){
+  debugger
+console.log("This.delted ====>",id)
+this.userDetails= this.userDetails.filter((x)=>x.id!==id)
+this.toastermessage=true
+setTimeout(() => {
+  this.toastermessage=false
+}, 2000);
+}
+checkout:any=''
+getByDepartment(event:any){
+  debugger
+  this.fillter=event.target.value
+
+
+}
+getbySearch(){
+  if(this.search.length>=2){
+
+  }
+}
+closeToaster(){
+  this.toastermessage=false
+}
 }
