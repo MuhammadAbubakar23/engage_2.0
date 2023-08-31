d5f2d5638c4751b6ea34c7a9dac345ee88b48130
TaimoorWajid <syedtaimoor26@gmail.com>
2023-08-31 20:01:39 +0500

    home component added in console and analytics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,44 +3,56 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './identity/AuthService/auth.guard';
 import { ModulesResolver } from './shared/resolver/modules.resolver';
 
-
 const routes: Routes = [
- {
-   path:'',
-   redirectTo:'/all-inboxes/my_inbox',
-   pathMatch:'full',
- },
- {
-  path:'all-inboxes',
-  redirectTo:'/all-inboxes/my_inbox',
-  pathMatch:'full',
-},
   {
     path: '',
-    loadChildren: () => import('./identity/identity.module').then(f => f.IdentityModule)
+    redirectTo: '/all-inboxes/my_inbox',
+    pathMatch: 'full',
   },
   {
-    path:'',
-    loadChildren : () => import('./modules/modules.module').then(f=>f.ModulesModule), canActivate: [AuthGuard],
-    data: {preload: true},
+    path: 'all-inboxes',
+    redirectTo: '/all-inboxes/my_inbox',
+    pathMatch: 'full',
+  },
+  {
+    path: 'console',
+    redirectTo: '/console/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'analytics',
+    redirectTo: 'analytics/report-designer',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./identity/identity.module').then((f) => f.IdentityModule),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./modules/modules.module').then((f) => f.ModulesModule),
+    canActivate: [AuthGuard],
+    data: { preload: true },
     resolve: {
-      modeuls: ModulesResolver
+      modeuls: ModulesResolver,
     },
   },
   {
-    path:'survey',
-    loadChildren : () => import('./survey-forms/survey-forms.module').then(f=>f.SurveyFormsModule),
+    path: 'survey',
+    loadChildren: () =>
+      import('./survey-forms/survey-forms.module').then(
+        (f) => f.SurveyFormsModule
+      ),
   },
   // { path: 'administration', loadChildren: () => import('./administration/administration.module').then(m => m.AdministrationModule) },
-  // { path: '404', component : NotFoundComponent}, 
+  // { path: '404', component : NotFoundComponent},
   // { path: '**', redirectTo: '/404', pathMatch: 'full'},
-
-
-
-]
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
