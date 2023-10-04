75f7f70fe595fed997978f41d49e899ec9b70e0c
TaimoorWajid <syedtaimoor26@gmail.com>
2023-10-04 18:37:28 +0500

    blank lines removed

--- src/app/modules/analytics/analytics.module.ts
+++ src/app/modules/analytics/analytics.module.ts
@@ -4,20 +4,12 @@ import { AnalyticsRoutingModule } from './analytics-routing.module';
 import { AnalyticsComponent } from './analytics.component';
 import { ExecutiveDashboardHeaderComponent } from './analytics-headers/executive-dashboard-header/executive-dashboard-header.component';
 import { LiveMonitoringHeaderComponent } from './analytics-headers/live-monitoring-header/live-monitoring-header.component';
-
 import { LayoutsModule } from 'src/app/layouts/layouts.module';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { NgChartsModule } from 'ng2-charts';
-import { ReportDbSettingsComponent } from './components/report-db-settings/report-db-settings.component';
-import { ReportlistingComponent } from './components/reportlisting/reportlisting.component';
 import { ExcelService } from './services/excel.service';
 import {DragDropModule} from '@angular/cdk/drag-drop';
-import { ReportDesignerComponent } from './components/report-designer/report-designer.component';
-
-
-// import {DndModule} from 'ngx-drag-drop';
 
 @NgModule({
   declarations: [
@@ -33,8 +25,7 @@ import { ReportDesignerComponent } from './components/report-designer/report-des
     NgSelectModule,
     NgChartsModule,
     DragDropModule,
-    ReactiveFormsModule,
-    // DndModule
+    ReactiveFormsModule
   ],
   providers: [ExcelService,
   DatePipe]
